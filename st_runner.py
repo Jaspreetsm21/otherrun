916c43fdaea22b54ed3ed0c2131596062f86a3fc
Jaspreet singh <60583082+Jaspreetsm21@users.noreply.github.com>
2020-06-06 12:10:55 +0100

    Update st_runner.py

--- st_runner.py
+++ st_runner.py
@@ -11,7 +11,7 @@ st.title('Liverpool Premier League: Journey to Winning the First Title in 30 Yea
 st.write("The purpose of this dashboard is to look at Liverpool's journey in the last decade toward winning their first Premier league title in 30 Years. Using the data, I wanted to explore what things has contributed toward this success.")
 @st.cache
 def load_data(nrows):
-    data = pd.read_csv('liverpool.csv',index_col=0,nrows=nrows)
+    data = pd.read_csv('liverpool.csv',nrows=nrows)
     return data
 
 data = load_data(10000)
